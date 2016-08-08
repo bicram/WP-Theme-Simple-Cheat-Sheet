a9fa7afcbcfe49d6a33fc9b099a473300b62fe62
Showhan Ahmed <showhanahmed@gmail.com>
2016-08-08 21:08:49 +0600

    HTML5 Doctype modified

--- WordPress Theme Development sheet.php
+++ WordPress Theme Development sheet.php
@@ -1,7 +1,7 @@
 <?php
 /* This code is for html to wordpress convert(only homepage) */
 /* <=================================================================> */
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes('xhtml'); ?>>
 	<head>
 		<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
